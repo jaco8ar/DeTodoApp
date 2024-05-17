b473a008c7455b8094af991ad792b0589b20b6f6
Jacobo Ochoa <jochoara@unal.edu.co>
2024-05-16 23:03:58 -0500

    class: NoteList annotations class added

--- src/main/java/classes/NoteList.java
+++ src/main/java/classes/NoteList.java
@@ -1,8 +1,72 @@
 package main.java.classes;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "notelist")
 public class NoteList {
-	ArrayList<Note> Notes;
+	@Id
+	@Column (name = "nlcode")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int noteListCode;
+	
+	@ManyToOne
+	@JoinColumn (name = "owner", referencedColumnName = "username")
+	private User owner;
+	
+	@Column (name = "nlname")
+	private String noteListName;
+	
+	@Column (name = "isrootnl")
+	private Boolean isRootNoteList;
+	
+	@Column (name = "path")
+	private String picPath;
+	
+	@OneToMany(mappedBy = "note")
+	private List<Note> Notes;
+	
+	
+	
+	
+	public int getNoteListCode() {
+		return noteListCode;
+	}
+	public void setNoteListCode(int noteListCode) {
+		this.noteListCode = noteListCode;
+	}
+	public User getOwner() {
+		return owner;
+	}
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	public String getNoteListName() {
+		return noteListName;
+	}
+	public void setNoteListName(String noteListName) {
+		this.noteListName = noteListName;
+	}
+	public Boolean getIsRootNoteList() {
+		return isRootNoteList;
+	}
+	public void setIsRootNoteList(Boolean isRootNoteList) {
+		this.isRootNoteList = isRootNoteList;
+	}
+	public String getPicPath() {
+		return picPath;
+	}
+	public void setPicPath(String picPath) {
+		this.picPath = picPath;
+	}
+	public List<Note> getNotes() {
+		return Notes;
+	}
+	public void setNotes(List<Note> notes) {
+		Notes = notes;
+	}
+	
 	
 }
